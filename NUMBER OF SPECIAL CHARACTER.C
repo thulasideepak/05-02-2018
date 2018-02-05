d4fadef1c8552a364dc3464b5ea1512ba62f7fa2
thulasideepak <35525691+thulasideepak@users.noreply.github.com>
2018-02-05 16:25:02 +0530

    Create NUMBER OF SPECIAL CHARACTER.C
    count the number of special character

---
+++ NUMBER OF SPECIAL CHARACTER.C
@@ -0,0 +1,17 @@
+#include<iostream.h>
+int main()
+{
+int i,j=0;
+char a[100];
+cout<<"enter the string";
+cin>>a[100];
+for(i=0;i<100;i++)
+{
+is digit (a[100])
+n=0;
+else
+++n;
+}
+cout<<"num of numeric %d"<<j;
+getch();
+}
