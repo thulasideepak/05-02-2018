190b7008726091094dd1c175aeb0ce01b8420db7
thulasideepak <35525691+thulasideepak@users.noreply.github.com>
2018-02-05 15:46:15 +0530

    Create STRING IN NUMERIC.C
    string in numeric

---
+++ STRING IN NUMERIC.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("\nEnter the input");
+	scanf("%d",&a);
+	if(a>=0||a<0)
+	{
+		printf("\n yes");
+	}
+	else
+	{
+		printf("\n no");
+	}
+	return 0;
+}
